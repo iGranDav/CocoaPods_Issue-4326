8c61d634cfd3cb3bf1ea205ba82c62dfb7ee7bc2
David Bonnet <david.bonnet85@gmail.com>
2015-10-16 13:52:55 +0200

    Initial Commit

---
+++ CocoaPods_Issue#4326/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoaPods_Issue#4326
+//
+//  Created by David Bonnet on 16/10/2015.
+//  Copyright © 2015 Milestone Project Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
